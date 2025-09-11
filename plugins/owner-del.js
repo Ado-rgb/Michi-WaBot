d2912ccad8f0f03cd30b50fab5d14d835d3454c2
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-09-11 14:40:16 -0500

    Actualizar owner-del.js

--- plugins/owner-del.js
+++ plugins/owner-del.js
@@ -1,23 +1,23 @@
-//[##] Creado por GianPoolS
+//[##]Creado por GianPoolS - github.com/GianPoolS
+//[##]No quites los créditos
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn }) => {
     try {
-        if (!m.quoted) {
-            return m.reply('❌ Debes responder al mensaje que deseas eliminar con .del');
-        }
+        if (!m.quoted) return m.reply('❌ Debes responder al mensaje que deseas eliminar con .del');
 
         const msgId = m.quoted.key.id;
         const chatId = m.chat;
 
-        await conn.sendMessage(chatId, { delete: { id: msgId, remoteJid: chatId, fromMe: true } });
+        await conn.sendMessage(chatId, {
+            delete: { id: msgId, remoteJid: chatId, fromMe: m.quoted.key.fromMe }
+        });
 
     } catch (error) {
         console.log(error);
-        m.reply('❌ No se pudo eliminar el mensaje.');
+        m.reply('❌ Error al eliminar el mensaje:\n' + error.toString());
     }
 };
 
 handler.command = ['del'];
-handler.rowner = false;
-handler.owner = true;
+handler.rowner = true;
 export default handler;
